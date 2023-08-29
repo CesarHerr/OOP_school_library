52133fecfb321bccf3d70e314450e4bb165b5201
Cesar Herrera <cesarherr321@gmail.com>
2023-08-29 01:57:47 -0400

    Add class Person

---
+++ library.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_reader :id
+  attr_accessor :name
+  attr_accessor :age
+
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
+
+p persona = Person.new(9, "Cesar", false)
+p persona.can_use_services?
