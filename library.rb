73d65d8a6faee5e3fecac90121ea07da08694a7c
Cesar Herrera <cesarherr321@gmail.com>
2023-08-29 02:27:07 -0400

    Add Teacher class

--- library.rb
+++ library.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -23,8 +23,8 @@ end
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name, parent_permission, classroom)
-    super(age, name, parent_permission)
+  def initialize(age, name:, parent_permission:, classroom:)
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
@@ -33,7 +33,22 @@ class Student < Person
   end
 end
 
-p persona1 = Person.new(9, 'Cesar', false)
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(age, name:, parent_permission:, specialization:)
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+p persona1 = Person.new(39, name: 'Cesar', parent_permission: false)
 p persona1.can_use_services?
-p student1 = Student.new(18, 'Antonio', true, '4C')
+p student1 = Student.new(8, name: 'Antony', parent_permission: false, classroom: '4C')
 p student1.play_hooky
+p teacher1 = Teacher.new(28, name: 'Jim', parent_permission: false, specialization: 'Backend')
+p teacher1.can_use_services?
