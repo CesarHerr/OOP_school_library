69d4ad0d04390a545e94e03dac3b9a4f4a016641
Cesar Herrera <cesarherr321@gmail.com>
2023-08-30 17:33:35 -0400

    Fix capitalize and trimmer errors

--- trimmer.rb
+++ trimmer.rb
@@ -2,6 +2,6 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.slice(0, 4)
+    @nameable.correct_name.slice(0, 10)
   end
 end
