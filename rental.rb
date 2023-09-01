1d0c9c078046f3bfbce626275580f45e6bb3915f
Cesar Herrera <cesarherr321@gmail.com>
2023-08-31 23:05:37 -0400

    Fix constructor, getter and setter on rental class

--- rental.rb
+++ rental.rb
@@ -2,12 +2,15 @@ require_relative 'book'
 require_relative 'class_person'
 
 class Rental
-  attr_accessor :date
-  attr_reader :book, :person
+  attr_accessor :date, :book, :person
 
   def initialize(date, book, person)
     @date = date
+
     @book = book
+    book.rental << self
+
     @person = person
+    person.rental << self
   end
 end
