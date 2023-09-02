7b7f78a19e14d0eb47323de5693fa417de29ac6d
Cesar Herrera <cesarherr321@gmail.com>
2023-09-01 21:33:08 -0400

    Add create_teacher method

--- app.rb
+++ app.rb
@@ -68,3 +68,18 @@ class App
     @people.push(student)
     puts "\nThe student was Created successfully"
   end
+
+  def create_teacher
+    puts 'create teacher'
+    print 'Age:'
+    age = gets.chomp
+
+    print 'Name:'
+    name = gets.chomp
+    print 'Specialization:'
+
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @people.push(teacher)
+    puts "\nThe teacher was Created successfully"
+  end
