55596be430a3be8d3e8f3112e25119f63cd7f51c
Cesar Herrera <cesarherr321@gmail.com>
2023-09-04 18:11:10 -0300

    Update App class to refactor it

--- app.rb
+++ app.rb
@@ -4,13 +4,14 @@ require_relative 'book'
 require_relative 'class_person'
 require_relative 'rental'
 require_relative 'classroom'
+require_relative 'person_menu'
 
 class App
-  attr_accessor :books, :people, :rentals
+  attr_accessor :books, :people, :rentals, :people_creator
 
   def initialize
     @books = []
-    @people = []
+    @people = People.new
     @rentals = []
   end
 
@@ -26,62 +27,19 @@ class App
   end
 
   def list_people
-    if @people.empty?
-      puts "\nThis is empty!! :("
-    else
-      puts "\nList all people"
-      @people.each_with_index do |element, index|
-        puts "#{index}) [#{element.class}] Name:#{element.name}, Age:#{element.age}, ID:#{element.id}"
-      end
-    end
+    @people.list_people
   end
 
   def create_person
-    print "\nDo you want to create a student (1) or a teachet (2)? [Input the number]:"
-    people = gets.chomp
-
-    case people
-    when '1'
-      create_student
-    when '2'
-      create_teacher
-    else
-      puts 'Wrong number!'
-    end
+    @people.create_person
   end
 
   def create_student
-    puts 'create student'
-    print 'Age:'
-    age = gets.chomp
-
-    print 'Name:'
-    name = gets.chomp
-
-    print 'Classroom:'
-    classroom = gets.chomp
-    Classroom.new(classroom)
-
-    print 'Has parent permission? [y/n]:'
-    permission = gets.chomp.upcase == 'Y'
-    student = Student.new(age, classroom, name, parent_permission: permission)
-    @people.push(student)
-    puts "\nThe student was Created successfully"
+    @people.create_student
   end
 
   def create_teacher
-    puts 'create teacher'
-    print 'Age:'
-    age = gets.chomp
-
-    print 'Name:'
-    name = gets.chomp
-    print 'Specialization:'
-
-    specialization = gets.chomp
-    teacher = Teacher.new(age, specialization, name)
-    @people.push(teacher)
-    puts "\nThe teacher was Created successfully"
+    @people.create_teacher
   end
 
   def create_book
