1afacc8cbc0e9331cc72ca357235306f4dcf47d7
Ruthmy <ruthmy17@gmail.com>
2023-09-05 09:59:58 -0600

    Modified 'app.rb' -file- - Modified methods 'create_rental' and 'list_rentals'. - Updated the imports.

--- app.rb
+++ app.rb
@@ -1,4 +1,3 @@
-require_relative 'rental'
 require_relative 'person_menu'
 require_relative 'book_menu'
 require_relative 'rentals_menu'
@@ -37,33 +36,10 @@ class App
   end
 
   def create_rental
-    puts "\nCreate a rental"
-    puts "\nSelect  a book from the following list by number"
-    list_books
-    book_selected = @books[gets.chomp.to_i]
-    puts "\nSelect a person from the list by bumber (not id)"
-    list_people
-    person_selected = @people[gets.chomp.to_i]
-    print 'Date:'
-    date = gets.chomp.to_s
-    rental = Rental.new(date, book_selected, person_selected)
-    @rentals.push(rental)
-    puts 'Rental created successfully'
-
+    @rentals.create_rental
   end
 
   def list_rentals
-    if @rentals.empty?
-      puts "\nThis is empty!! :("
-    else
-      puts "\nList all rentals for a given person id."
-      print "\nID of person:"
-      person_id = gets.chomp.to_i
-      rental_by_id = @rentals.select { |element| element.person.id == person_id }
-      rental_by_id.each_with_index do |element, index|
-        puts "#{index}) Date:#{element.date}, Book:#{element.book.title} by #{element.book.author}"
-      end
-    end
-
+    @rentals.list_rentals
   end
 end
