2fe79f7b762e76937233993cb51d731c7a984a1a
Alexandre <alexsales001@hotmail.com>
2023-09-05 12:19:58 -0300

    create hash called options to map menu choices to their labels and actions

--- menu.rb
+++ menu.rb
@@ -1,46 +1,42 @@
 class Menu
-    def initialize
-      @book_menu = BookMenu.new
-      @people_menu = PeopleMenu.new
-      @rental_menu = RentalMenu.new
-    end
-  
-    def run
-      loop do
-        display_menu
-        choice = gets.chomp.to_i
-  
-        case choice
-        when 1
-          @book_menu.list_books
-        when 2
-          @people_menu.list_people
-        when 3
-          @people_menu.create_person
-        when 4
-          @book_menu.create_book
-        when 5
-          @rental_menu.create_rental
-        when 6
-          @rental_menu.list_rentals_by_person
-        when 7
-          puts "\nThanks for using this app! :)"
-          break
-        else
-          puts "\nWrong number, try again"
-        end
+  def initialize
+    @book_menu = BookMenu.new
+    @people_menu = PeopleMenu.new
+    @rental_menu = RentalMenu.new
+    @options = {
+      1 => { label: 'List all books', action: -> { @book_menu.list_books } },
+      2 => { label: 'List all people', action: -> { @people_menu.list_people } },
+      3 => { label: 'Create a people', action: -> { @people_menu.create_person } },
+      4 => { label: 'Create a book', action: -> { @book_menu.create_book } },
+      5 => { label: 'Create a rental', action: -> { @rental_menu.create_rental } },
+      6 => { label: 'List all rentals for a given person id', action: -> { @rental_menu.list_rentals_by_person } },
+      7 => { label: 'Exit', action: -> { exit_application } }
+    }.freeze
+  end
+
+  def run
+    loop do
+      display_menu
+      choice = gets.chomp.to_i
+
+      if @options.key?(choice)
+        @options[choice][:action].call
+      else
+        puts "\nWrong number, try again"
       end
-    end
-  
-    def display_menu
-      puts "\nPlease choose an option by entering a number"
-      puts "\n1 - List all books"
-      puts '2 - List all people'
-      puts '3 - Create a people'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
+
+      break if choice == 7
     end
   end
-  
+
+  private
+
+  def display_menu
+    puts "\nPlease choose an option by entering a number"
+    @options.each { |key, value| puts "#{key} - #{value[:label]}" }
+  end
+
+  def exit_application
+    puts "\nThanks for using this app! :)"
+  end
+end
