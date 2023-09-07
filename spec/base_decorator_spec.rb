ccf0b1af5622d0dd8350d21b07c5fa70a7a9fa91
Cesar Herrera <cesarherr321@gmail.com>
2023-09-07 19:47:07 -0300

    Update teacher, student, and  decorator tests

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -2,10 +2,12 @@ require_relative '../base_decorator'
 require_relative '../nameable'
 
 describe Decorator do
+  # Arrange
   let(:nameable_mock) { double(Nameable, correct_name: 'Samuel') }
   let(:decorator) { Decorator.new(nameable_mock) }
 
   it 'correct_name should return the correct name from the mock' do
+    # Act and Assert
     expect(decorator.correct_name).to eq('Samuel')
   end
 end
--- spec/class_student_spec.rb
+++ spec/class_student_spec.rb
@@ -1,30 +1,42 @@
 require_relative '../class_student'
 
 describe Student do
-  before(:each) do
-    @student = Student.new(18, 'Clase A', 'Lupin', '300', parent_permission: true)
-  end
+  context 'when creating a Student' do
+    # Arrange
+    let(:student) { Student.new(18, 'Clase A', 'Lupin', '300', parent_permission: true) }
 
-  it 'Creating a Student method should work correctly' do
-    expect(@student.age).to eq(18)
-    expect(@student.classroom).to eq('Clase A')
-    expect(@student.name).to eq('Lupin')
-    expect(@student.id).to eq('300')
-  end
+    it 'should have correct attributes' do
+      # Act
+      age = student.age
+      classroom = student.classroom
+      name = student.name
+      id = student.id
 
-  it 'should generate a hash representation of the student' do
-    expected_hash = {
-      class: 'Student',
-      age: 18,
-      name: 'Lupin',
-      id: '300',
-      classroom: 'Clase A',
-      parent_permission: true
-    }
-    expect(@student.to_hash).to eq(expected_hash)
-  end
+      # Assert
+      expect(age).to eq(18)
+      expect(classroom).to eq('Clase A')
+      expect(name).to eq('Lupin')
+      expect(id).to eq('300')
+    end
 
-  it 'should return the play_hooky message' do
-    expect(@student.play_hooky).to eq('¯(ツ)/¯')
+    it 'should generate a hash representation of the student' do
+      # Act
+      student_hash = student.to_hash
+
+      # Assert
+      expected_hash = {
+        class: 'Student',
+        age: 18,
+        name: 'Lupin',
+        id: '300',
+        classroom: 'Clase A',
+        parent_permission: true
+      }
+      expect(student_hash).to eq(expected_hash)
+    end
+
+    it 'should return the play_hooky message' do
+      expect(student.play_hooky).to eq('¯(ツ)/¯')
+    end
   end
 end
