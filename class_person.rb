017db1b7207f802b55772aa353c5d4ec548a7987
Alexandre <alexsales001@hotmail.com>
2023-09-07 18:32:14 -0300

    Add RSpec tests for the person class

--- class_person.rb
+++ class_person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 require_relative 'rental'
 
-class Person < Nameable
+class Person 
   attr_accessor :name, :age, :id, :rental
 
   def initialize(age, id = nil, name = 'Unknown', parent_permission: true)
@@ -10,7 +10,6 @@ class Person < Nameable
     @age = age
     @parent_permission = parent_permission
     @rental = []
-    super()
   end
 
   def to_hash
