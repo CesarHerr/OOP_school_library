8dca90b32c3a2db3a7357cad850a582119f85f4a
Ruthmy <ruthmy17@gmail.com>
2023-09-06 23:36:48 -0600

    Modified 'rentals_menu.rb' -file- Added the test json files - Added the 'loading_rentals' method to the 'RentalMenu' class. - Aded files 'books.json', 'people.json' and 'rentals.json' as test info

--- rentals_menu.rb
+++ rentals_menu.rb
@@ -50,4 +50,16 @@ class RentalMenu
 
     File.write('rentals.json', JSON.pretty_generate(rental_data))
   end
+
+  def loading_rentals
+    return unless File.exist?('rentals.json')
+
+    rentals_data = JSON.parse(File.read('rentals.json'))
+    rentals_data.each do |rental|
+      book = Book.new(rental['book']['title'], rental['book']['author'])
+      person = Person.new(rental['person']['age'], rental['person']['id'],
+                          rental['person']['name'])
+      @rentals.push(Rental.new(rental['date'], book, person))
+    end
+  end
 end
